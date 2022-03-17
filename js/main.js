9e9b2189154b705861400151d108e05942b77f1b
Varshith Raj Basa <varshithraj.basa@gmail.com>
2022-03-17 22:58:10 +0530

    Download Customized QR Code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-var Links = {
-  Home: document.querySelector(".home"),
-  Generate: document.querySelector(".generate"),
-  About: document.querySelector(".about"),
+const getElement = (el) => {
+  return document.querySelector(el);
 };
-var inputData = document.querySelector('input#qrCodeData');
-var qr = document.querySelector('#qrcode');
+var Links = {
+  Home: getElement(".home"),
+  Generate: getElement(".generate"),
+  About: getElement(".about"),
+};
+var inputData = getElement("input#qrCodeData");
+var qr = getElement("#qrcode");
 
 const generate = () => {
   console.log("generate");
@@ -21,28 +24,38 @@ const changeActive = (path) => {
 };
 
 window.onload = function () {
-    if(!window.location.hash)
-        Links['Home'].click();
+  if (!window.location.hash) Links["Home"].click();
 };
 
 window.onhashchange = function () {
   let path = window.location.hash;
-  changeActive(path)
+  changeActive(path);
 };
 
 const generateQrCode = () => {
-    qr.innerHTML = null;
-    if(!inputData.value){
-        alert('Please enter a text or url');
-        inputData.focus();
-        return
-    }
-    var qrcode = new QRCode("qrcode", {
-        text: inputData.value,
-        width: 256,
-        height: 256,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-    });
-}
+  qr.innerHTML = null;
+  if (!inputData.value) {
+    alert("Please enter a text or url");
+    inputData.focus();
+    return;
+  }
+  const qrcode = new QRCode("qrcode", {
+    text: inputData.value,
+    width: 256,
+    height: 256,
+    colorDark: getElement("#colorInput2").value,
+    colorLight: getElement("#colorInput1").value,
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+  getElement("#button-download").disabled = false;
+};
+
+const downloadQrCode = () => {
+  let a = document.createElement("a");
+  a.setAttribute("href", getElement("img").src);
+  a.setAttribute("download", "QR Code.png");
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
